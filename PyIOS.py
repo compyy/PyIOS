4191bf3c316215fff5a7d8eed665af2e7f8b34e6
compyy <yasirz_cm@yahoo.com>
2015-07-09 12:17:56 +0200

    Update PyIOS.py

--- PyIOS.py
+++ PyIOS.py
@@ -3,20 +3,24 @@
 Author: Yasir Ashfaque
 Date: 	06/03/2014
 """
-
-from multiprocessing import Pool
+#################
+from multiprocessing import Pool, freeze_support
 import pexpect
 import getpass
 import time
 import argparse
 import sys, os
 from argparse import RawTextHelpFormatter
+import concurrent.futures
+#################
 
 ##Doc Strings##
 desc="""PyIOS is a custom python script to do ssh/telnet on CISCO routers and perform multiple tasks.
 The Script runs in multiple modes:
 It can run to implement/log the show/config sequential commands on the router.
 It can run in Analyse mode where it needs to collect output of show commands/configurations from routers and apply logic to the data collected and generate desired output.
+
+It requires Python 3.4+ and Pexpect 4.0+
 
 """
 
@@ -57,11 +61,11 @@ c3
 
 p_help="""By Default Script will open 10 processes parallel, it can be increased to desired value by using this switch.
 	"""
-##end of Strings	
+##End of DOC Strings	
 
 ##Definging Execution Function##############
 
-def openssh(username, password, hostname):
+def openssh(hostname, username, passwd):
 	ssh_newkey = "Are you sure you want to continue connecting (yes/no)?"
 	constr = "ssh " + username + "@" + hostname
 	ssh = pexpect.spawnu(constr)
@@ -80,7 +84,7 @@ def openssh(username, password, hostname):
 			print ("Could not accept new key from " + hostname + ", Try to do manual ssh first or remove the old key with ssh-keygen")
 			return 0
 	
-	ssh.sendline(password)
+	ssh.sendline(passwd)
 	auth = ssh.expect(["[P|p]assword:", ">", en_hostname])
 	
 	if auth == 0:
@@ -90,7 +94,7 @@ def openssh(username, password, hostname):
 	if auth == 1:
 		ssh.sendline("enable")
 		ssh.expect("[pP]assword:")
-		ssh.sendline(password)
+		ssh.sendline(passwd)
 		enable = ssh.expect(["[P|p]assword:", en_hostname])
 		
 		if enable == 0:
@@ -100,13 +104,18 @@ def openssh(username, password, hostname):
 	return ssh
 			
     
-def coderun(username, password, hostname,cmd):
-	ssh = openssh(username, password, hostname)
+def coderun(arguments):
+	args = (arguments.split("."))
+	hostname = args[0]
+	username = args[1]
+	passwd = args[2]
+	cmd = args[3]
+	ssh = openssh(hostname, username, passwd)
 	en_hostname = hostname + "#"
 	
 	if ssh!=0:
 		logf = open("logs/" +hostname, "w")
-		if type(cmd) == type("string"):
+		if cmd != "cmd-file":
 			ssh.sendline("terminal length 0")
 			ssh.expect(en_hostname,timeout=120)
 			ssh.sendline(cmd)
@@ -114,6 +123,7 @@ def coderun(username, password, hostname,cmd):
 			logf.write(ssh.before)
 			
 		else:
+			cmd = open("cmd.cfg", "r")
 			ssh.sendline("terminal length 0")
 			ssh.expect([en_hostname],timeout=120)
 			for i in cmd:
@@ -133,44 +143,42 @@ def coderun(username, password, hostname,cmd):
 		print ("CMD executed, on  " + hostname +" script is exiting..!")
 		ssh.close()
 
-def testrun(username, password, hostname,cmd):
-	print (username)
-	print (hostname)
-	print (password)
-	print (cmd)
-	
+def testrun(arguments):
+	print (arguments)
+	args = (arguments.split("."))
+	print (args[0])
+	print (args[1])
+	print (args[2])
+	print (args[3])
 
-##
-if __name__ == "__main__":
 	
+def main():
 	parser = argparse.ArgumentParser(description=desc,formatter_class=RawTextHelpFormatter)
 	parser.add_argument("username", help=u_help)
-	parser.add_argument("-m", dest="hostname", help=m_help)
+	parser.add_argument("-m", dest="hostname", default=[line.strip() for line in open("hosts.txt", "r")],help=m_help)
 	parser.add_argument("-t", dest="telnet", default=False, action='store_true', help=t_help)
-	parser.add_argument("-c", dest="cmd", default=(open("cmd.cfg", "r")), help=c_help)
+	parser.add_argument("-c", dest="cmd", default="cmd-file", help=c_help)
 	parser.add_argument("-a", dest="adv", default=False, action='store_true', help=a_help)
 	parser.add_argument("-p", dest="max_parallel", default=10, type=int, help=p_help)
 	args = parser.parse_args()
+	args.passwd = getpass.getpass("Enter Password: ")
 	
-	passwd = getpass.getpass("Enter Password: ")
-#	print (args.username)
-#	print (args.hostname)
-#	print (args.cmd)
-#	print (args.telnet)
-#	print (args.adv)
-#	print (args.max_parallel)
-						
+	if type(args.hostname) is str:
+		arguments = ".".join((args.hostname, args.username, args.passwd, args.cmd))
+		coderun(arguments)
 	
-	#try:
-	coderun(args.username, passwd, args.hostname, args.cmd)
-	
-#	except NameError:
-#		host = [line.strip() for line in open("hosts.txt", "r")]
-#		pool = Pool(max_parallel)
-#		pool.map(testrun, args.username, 1)
-#		pool.close()
-#		pool.join()
-#
+	else:
+		arguments=[]
+		for i in range(0,(len(args.hostname))):
+			arguments.append(".".join((args.hostname[i], args.username, args.passwd, args.cmd)))
+		
+		with concurrent.futures.ProcessPoolExecutor(max_workers=args.max_parallel) as executor:
+			executor.map(coderun, arguments)
+
 ##Functions end here, start of main code#####
+##
 
 # Start of code
+if __name__ == "__main__":
+	freeze_support()
+	main()
